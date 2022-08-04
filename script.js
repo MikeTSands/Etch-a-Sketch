760b6e5cc9342d0f2f498b1806e4e3af5ee51b36
Mike Sands <sands.michael@hotmail.com>
2022-08-04 14:59:53 +0900

    add function to create grid and change background color when scrolled over

--- script.js
+++ script.js
@@ -1,19 +1,45 @@
 //create variables for later use
 let container = document.getElementById('container');
-console.log(container);
+
+
 
 //function to build a grid in the container
 
 function buildGrid(x){
     for (let rows = 0 ; rows < x ; rows++){
         for (let columns = 0 ; columns < x ; columns++){
-            container.append("<div class = 'grid'> </div>");
+            let child = document.createElement("div");
+            child.classList.add("grid");
+            container.append(child);
         };
     };
-    $(".grid").width(960/x);
-    $(".grid").height(960/x);
 }
 
+buildGrid(16);
 
 
+//creates function to color the etch-a-sketch
+function changeColor(){
+    let grids = document.getElementsByClassName('grid');
+    for (const grid of grids){
+        grid.addEventListener('mouseover', function(){
+            grid.style.backgroundColor =  "black";
+        })
+    }
+}
 
+changeColor();
+
+//need to make button work
+//function to refresh 16x16 grid
+function refreshGrid(){
+    let grids = document.getElementsByClassName('grid');
+    for (const grid of grids){
+        grid.style.backgroundColor = "white";
+        console.log("hello");
+    }
+
+}
+
+let button = document.getElementById("button");
+button.addEventListener('click', refreshGrid());
